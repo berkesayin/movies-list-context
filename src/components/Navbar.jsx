32bbdd66a3e305a40a6ed630cb0f4e534a489924
Berke Sayın <sayinberkesayin@gmail.com>
2023-11-26 16:02:01 +0300

    Use context data at Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
+import { MovieListContext } from "../contexts/MovieListContext";
+
 export const Navbar = () => {
+  const [movies, setMovies] = useContext(MovieListContext);
   return (
     <div>
-      <h3>Navbar - Number Of Movies: </h3>
+      <h3>Navbar - Number Of Movies: {movies.length} </h3>
     </div>
   );
 };
